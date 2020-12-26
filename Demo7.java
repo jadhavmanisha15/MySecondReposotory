1dcfe2b49eca512320f8240a477984433cdf0e0e
jadhavmanisha15 <manisha24jadhav@gmail.com>
2020-12-26 16:47:21 +0530

    eight commit

---
+++ Demo7.java
@@ -0,0 +1,15 @@
+public class Demo
+{
+	public static void main(String[] args)
+	{
+		for(int i=0;i<=10;i++)
+		{
+		System.out.println("Hello World");
+		System.out.println("This is new changes");
+		System.out.println("Hello baby");
+                System.out.println("Hello");
+                 System.out.println("Hello shona");
+
+		}
+	}
+}
